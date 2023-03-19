6102621b1870c65d023ab1aa09a5168571021456
kim_mon_ey <113882868+kimmoney@users.noreply.github.com>
2023-03-20 02:06:48 +0900

    Add get file version

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import pefile
+import os
+import requests
+from urllib.parse import urlparse
+from win32api import GetFileVersionInfo, LOWORD, HIWORD
 
-pe = pefile.PE(r'gongzone_keep_point.exe')
+def get_version_git ():
+    info = GetFileVersionInfo ('gongzone_keep_point.exe', "\\")
+    ms = info['FileVersionMS']
+    ls = info['FileVersionLS']
+    return HIWORD (ms), LOWORD (ms), HIWORD (ls), LOWORD (ls)
 
-FileVersion = pe.FileInfo[0]#.StringTable[0].entries['FileVersion']
-print(FileVersion)
+r = requests.head("https://github.com/kimmoney/gongzone_releases/releases/latest")
+ver = r.headers['location'][-6:]
+if ver == get_version_git() :
+    pass
+else:
+    url = "https://github.com/kimmoney/gongzone_releases/releases/download/{}/gongzone_keep_point.exe".format(ver)
+    file = requests.get(url,stream = True)
+    parsed_file = urlparse(url)
+    file_name = os.path.basename(parsed_file.path)
+    file = requests.get(url)
+    open('C:/gongzone_keep_point.exe', 'wb').write(file.content)
